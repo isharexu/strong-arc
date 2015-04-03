4000bd9d4063d82b2cb3537f97e5ef4410e60e62
Anthony Ettinger <anthony@strongloop.com>
2015-04-03 15:10:44 -0700

    stub for chart (level 2)

--- client/www/scripts/modules/metrics/api/metrics.api.controller.js
+++ client/www/scripts/modules/metrics/api/metrics.api.controller.js
@@ -3,6 +3,55 @@ Metrics.controller('MetricsApiController', [
     '$state',
     '$log',
     '$interval',
-    function($scope, $state, $log, $interval) {
+    '$timeout',
+    'ChartConfigService',
+    function($scope, $state, $log, $interval, $timeout, ChartConfigService) {
         $log.log('api metrics');
+        var options = getChartOptions();
+        var data = getChartData();
+
+        $scope.chart = {
+            data: data,
+            options: options
+        };
+
+        function getChartOptions(){
+            return ChartConfigService.getChartOptions();
+        }
+
+
+        function getChartData(){
+            var data = [];
+            var colors = {
+                'GET': '#f00',
+                'POST': '#0f0',
+                'PUT': '#00f',
+                'DELETE': '#ff0'
+            };
+
+            var verbs = ['GET', 'POST', 'PUT', 'DELETE'];
+
+            verbs.forEach(function(verb){
+                var i = 0;
+                var values = [];
+                var now = moment();
+
+                while ( i < 10 ) {
+                    values.push({
+                        x: moment(now).add(i, 'minutes'),
+                        y: Math.round(Math.random() * 10)
+                    });
+
+                    i++;
+                }
+
+                data.push({
+                    values: values,      //values - represents the array of {x,y} data points
+                    key: verb, //key  - the name of the series.
+                    color: colors[verb]  //color - optional: choose your own line color.
+                });
+            });
+
+            return data;
+        }
     }]);
