059526b17757ddd36e5ab08cd2052997d6dbd3bd
Anthony Ettinger <anthony@strongloop.com>
2015-04-24 12:14:22 -0700

    add level 1,2,3 charts for api metrics

--- client/www/scripts/modules/metrics-api/metrics.api.controllers.js
+++ client/www/scripts/modules/metrics-api/metrics.api.controllers.js
@@ -2,16 +2,41 @@ ApiMetrics.controller('MetricsApiController', [
     '$scope',
     '$state',
     '$log',
+    '$q',
     '$interval',
     '$timeout',
     'MetricsApiService',
-    function($scope, $state, $log, $interval, $timeout, MetricsApiService) {
-        $scope.apiChart = {};
+    function($scope, $state, $log, $q, $interval, $timeout, MetricsApiService) {
+      $scope.apiChart = {};
+      $scope.server = {};
 
       window.setScrollView('.common-instance-view-container');
 
-      MetricsApiService.getMetricsApiChartData()
+      $scope.onClickLoad = function(form){
+        $log.log('loading chart data...');
+        $scope.server.host = form.host.$viewValue;
+        $scope.server.port = form.port.$viewValue;
+
+        $scope.getData(null, 0, 0);
+      };
+
+      $scope.getData = function(d, i, depth, initialModel){
+        var def = $q.defer();
+
+        MetricsApiService.getMetricsApiChartDataByNode(d, i, depth, $scope.server, initialModel)
           .then(function(data){
-            $scope.apiChart.data = data.data;
+            $log.log('i', i);
+            var chart = {
+              data: data,
+              node: d,
+              nodeIdx: i
+            };
+
+            $scope.apiChart = chart;
+
+            def.resolve($scope.apiChart);
           });
+
+        return def.promise;
+      };
     }]);
--- client/www/scripts/modules/metrics-api/metrics.api.services.js
+++ client/www/scripts/modules/metrics-api/metrics.api.services.js
@@ -5,7 +5,78 @@ ApiMetrics.service('MetricsApiService', [
     var svc = this;
 
     svc.getMetricsApiChartData = function(){
-      return $http.get('/scripts/modules/metrics-api/data.json');
+      return $http.get('/scripts/modules/metrics-api/data2.json');
+    };
+
+    svc.getMetricsApiChartDataByNode = function(d, i, depth, server, initialModel){
+      var api = 'http://'+server.host + ':' + server.port + '/api';
+      var endpoint;
+
+      switch(depth){
+        case 0:
+              endpoint = '/ExpressUsageRecords/dailySummary';
+              break;
+        case 1:
+              var modelName = d.name;
+              endpoint = '/ExpressUsageRecords/hourlySummary?modelOrUri='+modelName;
+              break;
+        case 2:
+              var modelName = initialModel;
+              var timeStamp = d.orig.timeStamp;
+              endpoint = '/ExpressUsageRecords/endpointDetail?modelOrUri='+modelName+'&windowStartTime='+timeStamp;
+              break;
+      }
+
+      var url = api+endpoint;
+
+      return $http.get(url)
+        .then(function(res){
+          $log.log(res);
+          var chartData = {
+            name: "flare",
+            children: []
+          };
+
+          if ( depth === 0 ) {
+            //convert data to d3 chart data
+            Object.keys(res.data).map(function(item){
+              chartData.children.push({
+                name: item,
+                size: res.data[item],
+                orig: item
+              });
+            });
+          } else if ( depth === 1 ) {
+            var hourly = res.data;
+
+            hourly.map(function(item){
+              var total = item.GET + item.POST + item.PUT + item.DELETE;
+              var obj = {
+                name: moment(item.timeStamp).format(' ha'),
+                size: total,
+                orig: item
+              };
+
+              chartData.children.push(obj);
+            });
+
+          } else if ( depth === 2 ) {
+            $log.log(res.data);
+            var endpoints = res.data;
+
+            endpoints.map(function(item){
+              var obj = {
+                name: item.requestUrl,
+                size: item.responseDuration,
+                orig: item
+              };
+
+              chartData.children.push(obj);
+            })
+          }
+
+          return chartData;
+        });
     };
 
     return svc;
